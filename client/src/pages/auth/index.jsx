af9a08b045ac28aff823b811f6720bf4437a87b3
Geddam Shyam Gowtham <137587586+nameishyam@users.noreply.github.com>
2024-08-31 23:13:33 +0530

    chore: Update npm dependencies and add Zustand for state management

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -8,11 +8,14 @@ import { Button } from "@/components/ui/button"
 import { toast } from "sonner";
 import apiClient from "@/lib/api-client";
 import { LOGIN_ROUTE, SIGNUP_ROUTE } from "@/utils/constants"
+import { useNavigate } from "react-router-dom"
 
 const Auth = () => {
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const validateSignup = () => {
         if (!email.length) {
@@ -49,6 +52,14 @@ const Auth = () => {
                 { email, password },
                 { withCredentials: true }
             );
+            if (response.data.user.id) {
+                if (response.data.user.profileSetup) {
+                    navigate("/chat");
+                }
+                else {
+                    navigate("/profile");
+                }
+            }
             console.log({ response });
         }
 
@@ -61,6 +72,9 @@ const Auth = () => {
                 { email, password },
                 { withCredentials: true }
             );
+            if (response.status === 201) {
+                navigate("/profile");
+            }
             console.log({ response });
         }
     };
@@ -74,10 +88,10 @@ const Auth = () => {
                             <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
                             <img src={Victory} alt="Victory Emoji" className="h-[100px]" />
                         </div>
-                        <p className="font-medium text-center">Fill in the details to get started with the best chat app!</p>
+                        <p className="font-medium text-center">Fill in the details to get started with chat application!</p>
                     </div>
                     <div className="flex items-center justify-center w-full">
-                        <Tabs className="w-3/4">
+                        <Tabs className="w-3/4" defaultValue="login">
                             <TabsList className="bg-transparent rounded-none w-full">
                                 <TabsTrigger value="login"
                                     className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
---
+++ client/src/store/index.js
@@ -0,0 +1,6 @@
+import { create } from "zustand";
+import { createAuthSlice } from "./slices/auth-slice";
+
+export const useAppStore = create()((...a) => ({
+    ...createAuthSlice(...a),
+}));
