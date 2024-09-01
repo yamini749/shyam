b12933f45599e70a82e1ceff15f5d1b9d4b31956
Geddam Shyam Gowtham <137587586+nameishyam@users.noreply.github.com>
2024-09-01 17:11:25 +0530

    chore: Update package dependencies for @radix-ui/react-avatar and react-icons

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,5 +1,11 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { getColor } from "@/lib/utils";
 import { useAppStore } from "@/store"
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { IoArrowBack } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+import { FaPlus, FaTrash } from "react-icons/fa"
+import { Input } from "@/components/ui/input";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -10,14 +16,83 @@ const Profile = () => {
   const [hovered, setHovered] = useState(false);
   const [selectedColor, setSelectedColor] = useState(0);
 
-  const saveChanges = () => {
-    
+  const saveChanges = async () => {
+
   }
 
 
-  return <div>Profile
-    <div>Email: {userInfo.email}</div>
-  </div>;
+  return (
+    <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
+      <div className="flex flex-col gap-10 w-[80vw] md:w-max">
+        <div>
+          <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="h-full w-32 md:w-48 md:h-48 relative flex items-center justify-center"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          >
+            <Avatar className="h-32 w-32 md:w-48 md:h-48 rounded-full overflow-hidden">
+              {image ? (
+                <AvatarImage
+                  src={image}
+                  alt="profile"
+                  className="object-cover w-full h-full bg-black"
+                />
+              ) : (
+                <div className={`uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[ipx] flex items-center justify-center rounded-full ${getColor(
+                  selectedColor
+                )}`}>
+                  {firstName
+                    ? firstName.split("").shift()
+                    : userInfo.email.split("").shift()}
+                </div>
+              )}
+            </Avatar>
+            {hovered && (
+              <div className="absolute insert-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full ">
+                {image ? (
+                  <FaTrash className="text-white text-3xl cursor-pointer" />
+                ) : (
+                  <FaPlus className="text-white text-3xl cursor-pointer" />
+                )}
+              </div>
+            )}
+            {/* <input type="text" /> */}
+          </div>
+          <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center">
+            <div className="w-full">
+              <Input
+                placeholder="Email"
+                type="email"
+                disabled
+                value={userInfo.email}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full">
+              <Input
+                placeholder="Email"
+                type="email"
+                value={userInfo.email}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full">
+              <Input
+                placeholder="Email"
+                type="email"
+                value={userInfo.email}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  );
 }
 
 export default Profile;
