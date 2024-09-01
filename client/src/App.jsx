804fc2364c2acd93a2e5dc839fb8df8e15471bfe
Geddam Shyam Gowtham <137587586+nameishyam@users.noreply.github.com>
2024-09-01 09:48:25 +0530

    chore: Update App component to include private and authentication routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Children } from 'react'
 import { Button } from '@/components/ui/button';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Auth from '@/pages/auth';
@@ -6,23 +6,35 @@ import Chat from './pages/chat';
 import Profile from './pages/profile';
 import { useAppStore } from '@/store';
 
-const PrivateRoute = ({children}) => {
-  const {userInfo} = useAppStore();
+const PrivateRoute = ({ children }) => {
+  const { userInfo } = useAppStore();
   const isAuthenticated = !!userInfo;
-  return isAuthenticated ? children : <Navigate ro="/auth" />;
+  return isAuthenticated ? children : <Navigate to="/auth" />;
 };
 
-function App() {
+const AuthRoute = ({ children }) => {
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? <Navigate to="/auth" /> : children;
+};
+
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route path="/auth" element={
+          <AuthRoute>
+            <Auth />
+          </AuthRoute>} />
+        <Route path="/chat" element={
+          <PrivateRoute>
+            <Chat />
+          </PrivateRoute>} />
         <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Navigate to="/auth" />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App
