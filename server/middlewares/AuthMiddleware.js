246a26e763d9e33d692248d95b0fd3f650ea6f19
Geddam Shyam Gowtham <137587586+nameishyam@users.noreply.github.com>
2024-09-01 13:50:47 +0530

    chore: Update AuthMiddleware.js to handle token verification and user authentication

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -2,8 +2,18 @@ import jwt from "jsonwebtoken";
 
 
 export const verifyToken = (request, response, next) => {
-    console.log(request.cookies);
     const token = request.cookies.jwt;
-    console.log({ token });
+    if (!token) {
+        return response.status(401).send("Access Denied, You are not Authenticated.");
+    }
+    jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+        if (err) {
+            return response.status(403).send("Token is not Valid.");
+        }
+        request.userId = payload.userId;
+        next();
+    });
 };
+
+
 
--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { signup, login, getUserInfo } from "../controllers/AuthController.js";
-import { verifyToken } from "../middleware/AuthMiddleware.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const authRoutes = Router();
 
 authRoutes.post("/signup", signup);
 authRoutes.post("/login", login);
-authRoutes.get("/user-info", verifyToken, getUserInfo);
+authRoutes.get("/user-info",verifyToken, getUserInfo);
 
 
 
