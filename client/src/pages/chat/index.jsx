33ac99fe50a2d9ca2862262499dd864b443d56e1
Geddam Shyam Gowtham <137587586+nameishyam@users.noreply.github.com>
2024-09-01 10:25:56 +0530

    chore: Add user profile setup check in Chat component

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,9 +1,19 @@
+import { useAppStore } from "@/store";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
+const Chat = () => {
+    const { userInfo } = useAppStore();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!userInfo.profileSetup) {
+            toast("Please setup your profile first.");
+            navigate("/profile");
+        }
+    }, [userInfo, navigate]);
 
-function Chat() {
-    return (
-        <div>Chat</div>
-    )
-}
+    return <div>Chat</div>;
+};
 
 export default Chat
