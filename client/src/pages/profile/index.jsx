1f19735d5b9785ed02f30456388e8f429adb6f3c
Geddam Shyam Gowtham <137587586+nameishyam@users.noreply.github.com>
2024-09-01 14:19:59 +0530

    chore: Add user profile setup check and update profile page

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,7 +1,20 @@
 import { useAppStore } from "@/store"
+import { useState, useEffect } from "react";
 
 const Profile = () => {
-  const { userInfo } = useAppStore();
+  const navigate = useNavigate();
+  const { userInfo, setUserInfo } = useAppStore();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [image, setImage] = useState(null);
+  const [hovered, setHovered] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(0);
+
+  const saveChanges = () => {
+    
+  }
+
+
   return <div>Profile
     <div>Email: {userInfo.email}</div>
   </div>;
