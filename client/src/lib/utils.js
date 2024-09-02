8bf8e4b3293229914dc7d08b9bf8291447bc314e
Geddam Shyam Gowtham <137587586+nameishyam@users.noreply.github.com>
2024-09-02 20:38:46 +0530

    chore: Update package dependencies for react-lottie and react-icons

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,5 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import animationData from "@/assets/lottie-json";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
@@ -16,5 +17,11 @@ export const getColor = (color) => {
   if (color >= 0 && color < colors.length) {
     return colors[color];
   }
-  return colors[0]; // Fallback to the first color if out of range
+  return colors[0];
+};
+
+export const animationDefaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData,
 };
--- client/src/pages/chat/components/empty-chat-container/index.jsx
+++ client/src/pages/chat/components/empty-chat-container/index.jsx
@@ -1,8 +1,22 @@
+import { animationDefaultOptions } from "@/lib/utils";
+import Lottie from "react-lottie";
 
 const EmptyChatContainer = () => {
   return (
-    <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center hidden duration-1000 transition-all">EmptyChatContainer</div>
-  )
-}
+    <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center hidden duration-1000 transition-all">
+      <Lottie
+        isClickToPauseDisabled={true}
+        height={200}
+        width={200}
+        options={animationDefaultOptions}
+      />
+      <div className="text-opacity-80 text-white flex flex-col gap-5 items-cneter mt-10 lg:text-4xl text-3xl transition-all duration-300 text-center">
+        <h3 className="poppins-medium">
+          Hi <span className="text-purple-500">Welcome hhuuujjkkj</span>
+        </h3>
+      </div>
+    </div>
+  );
+};
 
 export default EmptyChatContainer;
